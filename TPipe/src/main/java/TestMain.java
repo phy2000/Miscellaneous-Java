ceca5abc452ccdcf7ceeb0b7e79ba83d960ff391
phy2000 <mr.paulyoung@yahoo.com>
2017-01-25 18:30:54 -0600

    Add TimedPipe project (TPipe)

---
+++ TPipe/src/main/java/TestMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author pyoung
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("TestMain.main");
+
+	}
+
+}
